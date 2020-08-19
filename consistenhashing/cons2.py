1914f6c2b6137e11d90a9d31cb7c04ff6553eaca
iAnatoly <anatoly.ivanov@gmail.com>
2020-08-19 11:25:58 -0700

    base ring impl

--- consistenhashing/cons2.py
+++ consistenhashing/cons2.py
@@ -2,40 +2,39 @@
 See https://docs.openstack.org/swift/latest/ring_background.html
 
 """
-
 from hashlib import md5
 
 NODE_COUNT = 100
 DATA_ID_COUNT = 10000000
-REPLICAS = 3
-REPLICA_STEP = 5
+VNODE_COUNT = 1000
 
-node_counts = [0] * NODE_COUNT
+vnode2node = []
+for vnode_id in range(VNODE_COUNT):
+    vnode2node.append(vnode_id % NODE_COUNT)
 
-print("Adding data to nodes: ", end='', flush=True)
+new_vnode2node = vnode2node.copy()
 
+new_node_id = NODE_COUNT
+vnodes_to_reassign = VNODE_COUNT / (NODE_COUNT + 1)
+
+while vnodes_to_reassign > 0:
+    for node_to_take_from in range(NODE_COUNT):
+        for vnode_id, node_id in enumerate(vnode2node):
+            if node_id == node_to_take_from:
+                vnode2node[vnode_id] = new_node_id
+                vnodes_to_reassign -= 1
+                break
+        if vnodes_to_reassign <= 0:
+            break
+
+moved_ids = 0
 for data_id in range(DATA_ID_COUNT):
-    s_data_id = str(data_id).encode('utf8')
+    hsh = int(md5(str(data_id).encode('utf8')).hexdigest(),16)
+    vnode_id = hsh % VNODE_COUNT
+    node_id = vnode2node[vnode_id]
+    new_node_id = new_vnode2node[vnode_id]
+    if node_id != new_node_id:
+        moved_ids += 1
 
-    hsh = int(md5(s_data_id).hexdigest(),16)
-    node_id = hsh % NODE_COUNT
-    node_counts[node_id] += 1
-
-    for j in range(1,REPLICAS):
-        node_counts[(node_id + j*REPLICA_STEP) % NODE_COUNT] += 1
-
-    #progress bar, please ignore:
-    if data_id % (DATA_ID_COUNT//20) == 0:
-        print(".",end='', flush=True)
-
-print(" Done.")
-print(node_counts)
-
-desired_count = (DATA_ID_COUNT / NODE_COUNT) * REPLICAS
-print(f'{desired_count}: Desired data ids per node')
-max_count = max(node_counts)
-over = 100.0 * (max_count - desired_count) / desired_count
-print(f'{max_count}: Most data ids on one node, {over} over')
-min_count = min(node_counts)
-under = 100.0 * (desired_count - min_count) / desired_count
-print(f'{min_count}: Least data ids on one node, {under} under')
+percent_moved = 100.0 * moved_ids / DATA_ID_COUNT
+print(f'{moved_ids} ids moved, {percent_moved}%')
